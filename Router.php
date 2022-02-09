165d22e12a0540e876d4bf4c7d823fa0f5ee0c74
Jonathan Rangel <jonathan.rangelrada@gmail.com>
2022-02-08 21:29:24 -0600

    Fix

--- Router.php
+++ Router.php
@@ -7,22 +7,15 @@ class Router
     public array $getRoutes = [];
     public array $postRoutes = [];
 
-    public function get($url, $fn)
-    {
+    public function get($url, $fn) {
         $this->getRoutes[$url] = $fn;
     }
 
-    public function post($url, $fn)
-    {
+    public function post($url, $fn) {
         $this->postRoutes[$url] = $fn;
     }
 
-    public function comprobarRutas()
-    {
-        
-        // Proteger Rutas...
-        session_start();
-
+    public function comprobarRutas() {
         $currentUrl = $_SERVER['REQUEST_URI'] === '' ? '/' :  $_SERVER['REQUEST_URI'] ;
         $method = $_SERVER['REQUEST_METHOD'];
 
@@ -32,7 +25,6 @@ class Router
             $fn = $this->postRoutes[$currentUrl] ?? null;
         }
 
-
         if ( $fn ) {
             // Call user fn va a llamar una función cuando no sabemos cual sera
             call_user_func($fn, $this); // This es para pasar argumentos
@@ -41,9 +33,7 @@ class Router
         }
     }
 
-    public function render($view, $datos = [])
-    {
-
+    public function render($view, $datos = []) {
         // Leer lo que le pasamos  a la vista
         foreach ($datos as $key => $value) {
             $$key = $value;  // Doble signo de dolar significa: variable variable, básicamente nuestra variable sigue siendo la original, pero al asignarla a otra no la reescribe, mantiene su valor, de esta forma el nombre de la variable se asigna dinamicamente
@@ -57,3 +47,4 @@ class Router
         include_once __DIR__ . '/views/layout.php';
     }
 }
+ 
--- includes/database.php
+++ includes/database.php
@@ -1,6 +1,11 @@
 <?php
 
-$db = mysqli_connect($_ENV['DB_HOST'], $_ENV['DB_USER'], $_ENV['DB_PASSWORD'], $_ENV['DB_DB']);
+$db = mysqli_connect(
+    $_ENV['DB_HOST'], 
+    $_ENV['DB_USER'], 
+    $_ENV['DB_PASSWORD'], 
+    $_ENV['DB_DB']);
+
 $db->set_charset("utf8");
 
 if (!$db) {
